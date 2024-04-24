d221baba63b90d3eaf1d8ef04ad33f60041a3bbf
Raquel Castro <raquelcastrobermudo@gmail.com>
2024-04-24 10:09:00 +0200

    map

--- main.js
+++ main.js
@@ -1,43 +1,46 @@
 'use strict';
 const tasks = [
-    { name: 'Recoger setas en el campo', completed:true },
-    { name: 'Comprar pilas', completed: true },
-    { name: 'Poner una lavadora de blancos', completed: true },
-    {
-      name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-      completed: false,
+    { name: 'Recoger setas en el campo', completed:true},
+    { name: 'Comprar pilas', completed: true},
+    { name: 'Poner una lavadora de blancos', completed: true},
+    { name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript', completed: false, 
     },
   ];
 
   let list = document.querySelector('.js_list_task');
 
-  function renderTask (listado){
-    for (const listado of tasks){
-      if (listado.completed === true){
+  function renderTask (){ //pintar en html
+  
+    for (const item of tasks){
+      
+      if (item.completed === true){
         list.innerHTML += `<li class= "task tachado"> <input type="checkbox" checked>
-    ${listado.name}</li>`;
+    ${item.name}</li>`;
     } else {
         list.innerHTML += `<li class= "task"> <input type="checkbox">
-    ${listado.name}</li>`;
+    ${item.name}</li>`;
       }    
   }
 };
 
   
-  renderTask(tasks);
-
-  const cadaTarea = document.querySelector('.task')
-  console.log(cadaTarea);
+  renderTask();
 
  
-
   // Hay que hacer que esta función modifique el array y su propiedad completed. 
   function handleClick(event){
-    let clicked = event.target;
-    list = " ";
-    console.log(list);
-    const tachado = tasks.map((listado) => listado.renderTask(clicked)); 
-    console.log(tachado);
+    const clicked = event.target;
+    const selectTask = tasks.completed.map((tarea) => tarea===clicked);
+    console.log(selectTask);
+   if (selectTask === clicked){
+    selectTask.completed = false;
+     
+   }else{
+    selectTask.completed = true;
+   } //pedir soporte para ver como solucionar el tachado (map)
+   ;
+    /* const tachado = tasks.map((listado) => listado.renderTask(clicked)); 
+    console.log(tachado); */
 
     // if (list === clicked){
     //   list.classList.remove('tachado');
