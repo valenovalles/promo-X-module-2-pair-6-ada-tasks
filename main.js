3ac8d26bd2a7e4083be877431ad95468350e1614
Raquel Castro <raquelcastrobermudo@gmail.com>
2024-04-26 14:04:36 +0200

    last pair

--- main.js
+++ main.js
@@ -9,28 +9,59 @@ const tasks = [
 
   let list = document.querySelector('.js_list_task');
 
+
   function renderTask (){ //pintar en html
   list.innerHTML= " ";
     for (const item of tasks){
       
       if (item.completed === true){
-        list.innerHTML += `<li class= "task tachado"> <input type="checkbox" checked>
-    ${item.name}</li>`;
-    } else {
-        list.innerHTML += `<li class= "task"> <input type="checkbox">
-    ${item.name}</li>`;
+        list.innerHTML += `<li class= "task tachado"> <input type="checkbox" class="js_input"  id=${item.id} checked>
+      ${item.name}</li>`;
+      } else {
+        list.innerHTML += `<li class= "task"> <input type="checkbox" class="js_input" id=${item.id}>
+      ${item.name}</li>`;
       }    
   }
+  
+
 };
 
-  
   renderTask(tasks);
 
- 
-  // Hay que hacer que esta función modifique el array y su propiedad completed. 
+  const checkBox = document.querySelectorAll('.js_input');
+
   function handleClick(event){
-    let clicked = event.target;
-    let selectTask = tasks.find((tarea) => tarea.name === clicked.textContent.trim());
+    let clicked = event.target.id;
+    console.log(clicked)
+    if(clicked){
+  tasks.completed !== tasks.completed;
+  }
+    
+  renderTask(tasks);
+    //cuando recojas la tarea clicada, me tienes que cambiar el completed. Si es true ponerlo false y si es false ponerlo true
+     
+     /* if(clickedTask.completed===true ){
+        task.completed = false;
+    }else{
+      task.completed = true;
+    } */
+    };
+  
+
+  
+    console.log(checkBox);
+
+  
+    list.addEventListener('click', handleClick);
+
+
+
+/* const task = checkBox.findIndex((task)=> task.id === parseInt(value))
+checkBox[task].completed !==  checkBox[task].completed */
+
+
+ 
+ /*    let selectTask = tasks.find((tarea) => tarea.id === clicked.id);
     // console.log(selectTask);
    if ( selectTask && selectTask.completed === true){
     console.log(selectTask.completed);
@@ -39,17 +70,21 @@ const tasks = [
    }else{
     console.log('es false');
     selectTask.completed = true;
-   }
+   }*/
+
+
+
+
+  // Hay que hacer que esta función modifique el array y su propiedad completed. 
+
   
   //pedir soporte para ver como solucionar el tachado (map)
-   ;
-   renderTask(selectTask);
+ 
     /* const tachado = tasks.map((listado) => listado.renderTask(clicked)); 
     console.log(tachado); */
 
     // if (list === clicked){
     //   list.classList.remove('tachado');
     // }
-  };
-  
-  list.addEventListener('click', handleClick);
+ 
+ 
